19559dc4d4beaff05a37353e0b59a267e549bcff
SpringMaple <springmaple@live.com.my>
2016-09-20 02:04:21 +0800

    update

--- app.js
+++ app.js
@@ -1,35 +1,56 @@
 const {app, BrowserWindow, ipcMain} = require('electron')
 const Datastore = require('nedb')
-const Util = require('./util.js')
+const Util = require('./lib/util.js')
 
 var mbrDb = new Datastore()
 var workingDate = Util.getNearestDate()
+
+var win = null
+var mbrNewWin = null
 
 app.on('browser-window-created', (e, window) => {
   window.setMenu(null)
 })
 
 app.on('ready', () => {
-  let win = new BrowserWindow({ width: 1000, height: 600 })
+  win = new BrowserWindow({ width: 1000, height: 600 })
   win.on('closed', () => {
     win = null
   })
 
-  win.loadURL(`file://${__dirname}/../view/html/mbr-ls.html`)
+  win.loadURL(`file://${__dirname}/front/html/mbr-ls.html`)
 })
+
+
+ipcMain.on('show-mbr-new', (evt, arg) => {
+  mbrNewWin = new BrowserWindow({
+    parent: win,  
+    modal: true,
+    maximizable: false,
+    minimizable: false,
+    width: 800
+  })
+  mbrNewWin.on('closed', () => {
+    mbrNewWin = null
+  })
+  mbrNewWin.loadURL(`file://${__dirname}/front/html/mbr-new.html`)
+})
+
 
 ipcMain.on('set-working-date', (evt, date) => {
   workingDate = date
+  win.webContents.send('ls-mbr-reload', null)
 })
 
 ipcMain.on('new-mbr', (evt, mbr) => {
   mbr._workingDate = workingDate
   mbrDb.insert(mbr, function (err, newMbr) {
     if (err !== null) 
-      rsp = err
-    else 
-      rsp = 'success'
-    rsp = evt.sender.send('new-mbr', rsp)
+      evt.sender.send('new-mbr', err)
+    else {
+      mbrNewWin.close()
+      win.webContents.send('ls-mbr-reload', null)
+    }
   })
 })
 
