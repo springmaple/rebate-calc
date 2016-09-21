461168e5e762e70fd9bbcb539fb43c9aabeefdf1
SpringMaple <springmaple@live.com.my>
2016-09-22 03:48:00 +0800

    update

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const {app, BrowserWindow, ipcMain} = require('electron')
 const Datastore = require('nedb')
 const Util = require('./lib/util.js')
+const htmlPath = `file://${__dirname}/front/html`
 
 var mbrDb = new Datastore({})
 var workingDate = Util.getNearestDate()
-
 var win = null
-var popupWin = null
+var popWin = null
+
+function lsMbr() {
+  mbrDb.find({ _startDate: { $lte: workingDate }, _doc: 'mbr' })
+    .sort({ mbrName: 1 })
+    .exec((err, mbrs) => {
+      win.webContents.send('ls-mbr', mbrs)
+    })
+}
+
+function onMbrChanged(err) {
+  if (err)
+    alert(err)
+  lsMbr()
+}
 
 app.on('browser-window-created', (e, window) => {
   window.setMenu(null)
@@ -17,88 +31,56 @@ app.on('ready', () => {
   win.on('closed', () => {
     win = null
   })
-
-  win.loadURL(`file://${__dirname}/front/html/mbr-ls.html`)
-})
-
-
-ipcMain.on('show-mbr-new', (evt, arg) => {
-  popupWin = new BrowserWindow({
-    parent: win,  
-    modal: true,
-    maximizable: false,
-    minimizable: false,
-    width: 800
-  })
-  popupWin.on('closed', () => {
-    popupWin = null
-  })
-  popupWin.loadURL(`file://${__dirname}/front/html/mbr-new.html`)
+  win.loadURL(`${htmlPath}/mbr-ls.html`)
 })
 
 ipcMain.on('show-mbr-info', (evt, _id) => {
-  popupWin = new BrowserWindow({
-    parent: win,  
+  popWin = new BrowserWindow({
+    parent: win,
     modal: true,
     maximizable: false,
     minimizable: false,
   })
-  popupWin.on('closed', () => {
-    popupWin = null
+  popWin.on('closed', () => {
+    popWin = null
   })
-  popupWin.loadURL(`file://${__dirname}/front/html/mbr-info.html?_id=${_id}`)
+  let url = `${htmlPath}/mbr-info.html`
+  if (_id)
+    url += `?_id=${_id}`
+  popWin.loadURL(url)
 })
 
 ipcMain.on('show-mbr-rebate', (evt, _id) => {
-  popupWin = new BrowserWindow({
-    parent: win,  
+  popWin = new BrowserWindow({
+    parent: win,
     modal: true,
     minimizable: false,
   })
-  popupWin.on('closed', () => {
-    popupWin = null
+  popWin.on('closed', () => {
+    popWin = null
   })
-  popupWin.loadURL(`file://${__dirname}/front/html/mbr-rebate.html?_id=${_id}`)
+  popWin.loadURL(`${htmlPath}/mbr-rebate.html?_id=${_id}`)
 })
 
 ipcMain.on('set-working-date', (evt, date) => {
   workingDate = date
-  win.webContents.send('ls-mbr-reload', null)
+  lsMbr()
 })
 
 ipcMain.on('new-mbr', (evt, mbr) => {
-  mbr._workingDate = workingDate
-  mbrDb.insert(mbr, function (err, newMbr) {
-    if (err !== null) 
-      evt.sender.send('new-mbr', err)
-    else {
-      popupWin.close()
-      win.webContents.send('ls-mbr-reload', null)
-    }
-  })
+  mbr._startDate = workingDate
+  mbr._doc = 'mbr'
+  mbrDb.insert(mbr, onMbrChanged)
 })
 
-ipcMain.on('ls-mbr', (evt, date) => {
-  if (date === null)
-    date = workingDate
-  
-  mbrDb.find({_workingDate: {$lte: date}}, (err, mbrs) => {
-    evt.sender.send('ls-mbr', mbrs)
-  })
+ipcMain.on('upd-mbr', (evt, obj) => {
+  mbrDb.update({ _id: obj._id }, { $set: obj.mbr }, {}, onMbrChanged)
 })
 
 ipcMain.on('get-mbr', (evt, _id) => {
-  mbrDb.findOne({_id: _id}, (err, mbr) => {
+  mbrDb.findOne({ _id: _id }, (err, mbr) => {
     evt.sender.send('get-mbr', mbr)
   })
 })
 
-ipcMain.on('upd-mbr', (evt, obj) => {
-  // obj: {_id: 'xxx', mbr: {...}}
-  mbrDb.update({_id: obj._id}, {$set: obj.mbr}, {returnUpdatedDocs: true}, 
-    function(err, numAffected, affectedDocuments, upsert) {
-      popupWin.close()
-      win.webContents.send('ls-mbr-reload', null)
-    }
-  )
-})
+ipcMain.on('ls-mbr', lsMbr)
