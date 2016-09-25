9a91c0fa099a47500e41d27b7fc77f3b7f9990f0
SpringMaple <springmaple@live.com.my>
2016-09-26 03:07:55 +0800

    update

--- src/app.js
+++ src/app.js
@@ -10,7 +10,10 @@ var popWin = null
 
 
 function lsMbr() {
-  mbrDb.find({ _startDate: { $lte: workingDate }, _doc: 'mbr' })
+  mbrDb.find({
+    _doc: 'mbr', _startDate: { $lte: workingDate }, _day: Util.splitDate(workingDate).d,
+    $or: [{ _endDate: null }, { _endDate: { $gt: workingDate } }]
+  })
     .sort({ mbrName: 1 })
     .exec((err, mbrs) => {
       win.webContents.send('ls-mbr', mbrs)
@@ -34,38 +37,35 @@ app.on('browser-window-created', (e, window) => {
 })
 
 app.on('ready', () => {
-  win = new BrowserWindow({ width: 1000, height: 600 })
+  win = new BrowserWindow({minWidth: 400, minHeight: 300, show: false})
+  win.maximize()
   win.on('closed', () => {
     win = null
   })
   win.loadURL(`${htmlPath}/mbr-ls.html`)
+  win.show()
 })
 
-ipcMain.on('show-mbr-info', (evt, _id) => {
+ipcMain.on('show-mbr-page', (evt, page) => {
   popWin = new BrowserWindow({
+    minWidth : 400,
+    minHeight: 300, 
+    show: false,
     parent: win,
     modal: true,
     minimizable: false,
   })
+  popWin.maximize()
   popWin.on('closed', () => {
     popWin = null
   })
-  let url = `${htmlPath}/mbr-info.html`
+
+  let {_id, name} = page
+  let url = `${htmlPath}/${name}.html`
   if (_id)
     url += `?_id=${_id}`
   popWin.loadURL(url)
-})
-
-ipcMain.on('show-mbr-rebate', (evt, _id) => {
-  popWin = new BrowserWindow({
-    parent: win,
-    modal: true,
-    minimizable: false,
-  })
-  popWin.on('closed', () => {
-    popWin = null
-  })
-  popWin.loadURL(`${htmlPath}/mbr-rebate.html?_id=${_id}`)
+  popWin.show()
 })
 
 ipcMain.on('set-working-date', (evt, date) => {
@@ -80,12 +80,18 @@ ipcMain.on('get-working-date', (evt, arg) => {
 
 ipcMain.on('new-mbr', (evt, mbr) => {
   mbr._startDate = workingDate
+  mbr._endDate = null
+  mbr._day = Util.splitDate(workingDate).d
   mbr._doc = 'mbr'
   mbrDb.insert(mbr, onMbrChanged)
 })
 
 ipcMain.on('upd-mbr', (evt, obj) => {
   mbrDb.update({ _id: obj._id }, { $set: obj.mbr }, {}, onMbrChanged)
+})
+
+ipcMain.on('del-mbr', (evt, _id) => {
+  mbrDb.update({ _id: _id }, { $set: {_endDate: workingDate} }, {}, onMbrChanged)
 })
 
 ipcMain.on('get-mbr', (evt, _id) => {
@@ -97,7 +103,7 @@ ipcMain.on('get-mbr', (evt, _id) => {
 ipcMain.on('ls-mbr', lsMbr)
 
 ipcMain.on('set-mbr-profit', (evt, profit) => {
-  query = { _doc: 'profit', _date: workingDate }
+  let query = { _doc: 'profit', _date: workingDate }
   if (profit === null)
     mbrDb.remove(query)
   else
@@ -110,6 +116,43 @@ ipcMain.on('set-mbr-profit', (evt, profit) => {
 
 ipcMain.on('get-mbr-profit', getProfit)
 
+ipcMain.on('set-rebate-mbr', (evt, rebateMbr) => {
+  let _doc = 'rebate-mbr'
+  let {mbr_id, mbrPct, mbr} = rebateMbr
+  let query = { _doc: _doc, mbr_id: mbr_id, mbrPct: mbrPct }
+  mbrDb.update(query, { $set: { mbr: mbr } }, {},
+    (err, numAff) => {
+      if (!numAff)
+        mbrDb.insert({ _doc: _doc, mbr_id: mbr_id, mbrPct: mbrPct, mbr: mbr })
+    })
+})
+
+ipcMain.on('set-rebate-package', (evt, rebatePackage) => {
+  let _doc = 'rebate-package'
+  let _date = workingDate
+  let {mbr_id, mbrPct, mbrPackage} = rebatePackage
+  let query = { _doc: _doc, _date: _date, mbr_id: mbr_id, mbrPct: mbrPct }
+  mbrDb.update(query, { $set: { mbrPackage: mbrPackage } }, {},
+    (err, numAff) => {
+      if (!numAff)
+        mbrDb.insert({ _doc: _doc, _date: workingDate, mbr_id: mbr_id, mbrPct: mbrPct, mbrPackage: mbrPackage })
+    })
+})
+
 ipcMain.on('ls-rebate', (evt, _id) => {
-  
+  for (let mbrPct = 3; mbrPct >= 1; mbrPct--) {
+    let queryMbr = { _doc: 'rebate-mbr', mbr_id: _id, mbrPct: mbrPct }
+    mbrDb.findOne(queryMbr, (err, rebateMbr) => {
+      if (rebateMbr)
+        evt.sender.send('ls-rebate-mbr', rebateMbr)
+    })
+
+    let queryPackage = { _doc: 'rebate-package', _date: workingDate, mbr_id: _id, mbrPct: mbrPct }
+    mbrDb.findOne(queryPackage, (err, rebatePackage) => {
+      if (rebatePackage)
+        evt.sender.send('ls-rebate-package', rebatePackage)
+      else
+        evt.sender.send('ls-rebate-package-no-data', mbrPct)
+    })
+  }
 })
