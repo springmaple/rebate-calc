9f470e3ce7f387b87741ea1765a34acf713d311a
SpringMaple <springmaple@live.com.my>
2016-10-07 03:24:38 +0800

    update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const {app, BrowserWindow, ipcMain, shell} = require('electron')
 const Datastore = require('nedb')
 const Util = require('./lib/util.js')
-const fs = require('fs');
+const fs = require('fs')
 const htmlPath = `file://${__dirname}/front/html`
 const oldDbPath = `${__dirname}\\..\\..\\calc.dat`
 const newDbPath = `${__dirname}\\..\\..\\calc.dat2`
@@ -10,121 +10,104 @@ var mbrDb = null
 var workingDay = Util.getNearestDay()
 var win = null
 var popWin = null
-var hasOldDb = false
 
-function initDb(callback) {
-  
+function initDb(doneCb) {
+  let doMigrate = true
   try {
     fs.accessSync(oldDbPath, fs.F_OK)
-    hasOldDb = true
   } catch (e) {
-    mbrDb = new Datastore({ filename: newDbPath, autoload: true })
-    callback()
+    doMigrate = false
   }
-  
-  if (hasOldDb) {
+
+  if (doMigrate) {
     try {
       fs.accessSync(newDbPath, fs.F_OK)
-      mbrDb = new Datastore({ filename: newDbPath, autoload: true })
-      callback()
-    } catch (e) {
-      let oldDb = new Datastore({ filename: oldDbPath, autoload: true })
-      mbrDb = new Datastore({ filename: newDbPath, autoload: true })
-      
-      let profitDat = {_id: 'profit', mbrProfit10: null, mbrProfit20: null, mbrProfit30: null}
-      oldDb.find({ _doc: 'profit' }, (evt, oldProfit) => {
-        for (let profit of oldProfit) {
-          let {m, d} = Util.splitDate(profit._date)
-          if (m == 9) {
-            let mbrProfit = Util.toFloat(profit.mbrProfit, 2)
-            if (isNaN(mbrProfit))
-              mbrProfit = null
-            profitDat['mbrProfit' + d] = mbrProfit
-          }
+      doMigrate = false
+    } catch (e) {}
+  }
+
+  mbrDb = new Datastore({ filename: newDbPath, autoload: true })
+  if (!doMigrate)
+    return doneCb()
+
+  let oldDb = new Datastore({ filename: oldDbPath, autoload: true })
+  let profitDat = { _id: 'profit', mbrProfit10: null, mbrProfit20: null, mbrProfit30: null }
+  oldDb.find({ _doc: 'profit' }, (evt, oldProfit) => {
+    for (let profit of oldProfit) {
+      let {m, d} = Util.splitDate(profit._date)
+      if (m == 9) {  // September 2016 only.
+        let mbrProfit = Util.toFloat(profit.mbrProfit, 2)
+        if (isNaN(mbrProfit))
+          mbrProfit = null
+        profitDat['mbrProfit' + d] = mbrProfit
+      }
+    }
+    mbrDb.insert(profitDat)
+    
+    oldDb.find({ _doc: 'mbr' }, (evt, oldMbrs) => {
+      let totalMbrs = oldMbrs.length
+      if (totalMbrs <= 0)
+        return doneCb()
+
+      let totalProcessed = 0
+      for (let mbr of oldMbrs) {
+        mbr._addDate = mbr._startDate
+        if (mbr._endDate)
+          mbr._rmDate = mbr._endDate
+        delete mbr._startDate
+        delete mbr._endDate
+        delete mbr._doc
+        for (let i=1; i<=3; i++) {
+          let mbrRebateLs = []
+          mbr['mbrRebate' + i] = mbrRebateLs
+          for (let j=0; j<30; j++)
+            mbrRebateLs.push({mbrDay: null, mbrName: null, mbrPackage: null})
         }
 
-        mbrDb.insert(profitDat)
-        
-        oldDb.find({ _doc: 'mbr' }, (evt, mbrs) => {
-          if (!mbrs) {
-            callback()
-          } else {
-            let totalMbrs = mbrs.length
-            let totalProcessed = 0
-
-            for (let mbr of mbrs) {
-              mbr._addDate = mbr._startDate
-              if (mbr._endDate)
-                mbr._rmDate = mbr._endDate
-              delete mbr._startDate
-              delete mbr._endDate
-              delete mbr._doc
-              
-              console.log('MBR:::::' + mbr)
-              
-              for (let i=1; i<=3; i++) {
-                let mbrRebateLs = []
-                mbr['mbrRebate' + i] = mbrRebateLs
-                for (let j=0; j<30; j++) {
-                  mbrRebateLs.push({mbrDay: null, mbrName: null, mbrPackage: null})
-                }
+        oldDb.find({ _doc: 'rebate-mbr', mbr_id: mbr._id }, (err, oldMbrRebates) => {
+          for (let oldMbrRebate of oldMbrRebates) {
+            let oldMbrRebateLs = oldMbrRebate.mbr
+            if (oldMbrRebateLs && oldMbrRebateLs.length > 0) {
+              let mbrRebateLs = mbr['mbrRebate' + oldMbrRebate.mbrPct]
+              for (let j=0; j<30; j++) {
+                let mbrRebate = mbrRebateLs[j]
+                mbrRebate.mbrName = oldMbrRebateLs[j].mbrName
+                mbrRebate.mbrDay = oldMbrRebateLs[j].mbrDate
               }
-              
-              oldDb.find({ _doc: 'rebate-mbr', mbr_id: mbr._id }, (err, mbrRebates) => {
-                for (let oldMbrRebate of mbrRebates) {
-                  let oldMbrRebateLs = oldMbrRebate.mbr
-                  if (oldMbrRebateLs) {
-                    let mbrRebateLs = mbr['mbrRebate' + oldMbrRebate.mbrPct]
-                    for (let j=0; j<30; j++) {
-                      mbrRebateLs[j].mbrName = oldMbrRebateLs[j].mbrName
-                      mbrRebateLs[j].mbrDay = oldMbrRebateLs[j].mbrDate
-                    }
-                  }
-                }
-                
-                oldDb.find({ _doc: 'rebate-package', mbr_id: mbr._id }, (err, mbrPackages) => {
-                  for (let oldMbrPackage of mbrPackages) {
-                    if (Util.splitDate(oldMbrPackage._date).m != 9)
-                      continue
-                    let oldMbrPackageLs = oldMbrPackage.mbrPackage
-                    if (oldMbrPackageLs) {
-                      let mbrRebateLs = mbr['mbrRebate' + oldMbrPackage.mbrPct]
-                      for (let j=0; j<30; j++) {
-                        mbrRebateLs[j].mbrPackage = oldMbrPackageLs[j]
-                      }
-                    }
-                  }
-                  
-                  mbrDb.insert(mbr)
-                  totalProcessed += 1
-                  if (totalProcessed == totalMbrs)
-                    callback()
-                })
-              })
             }
           }
+          
+          oldDb.find({ _doc: 'rebate-package', mbr_id: mbr._id }, (err, oldMbrPackages) => {
+            for (let oldMbrPackage of oldMbrPackages) {
+              if (Util.splitDate(oldMbrPackage._date).m != 9)  // September 2016 only.
+                continue
+              let oldMbrPackageLs = oldMbrPackage.mbrPackage
+              if (oldMbrPackageLs && oldMbrPackageLs.length > 0) {
+                let mbrRebateLs = mbr['mbrRebate' + oldMbrPackage.mbrPct]
+                for (let j=0; j<30; j++)
+                  mbrRebateLs[j].mbrPackage = oldMbrPackageLs[j]
+              }
+            }
+            
+            mbrDb.insert(mbr)
+            totalProcessed += 1
+            if (totalProcessed == totalMbrs)
+              doneCb()
+          })
         })
-
-      })
-      
-    }
-  }
+      }
+    })
+  })
 }
 
 function lsMbr() {
   mbrDb.find({ _day: workingDay, _rmDate: { $exists: false } })
-    .sort({ mbrName: 1 })
-    .exec((err, mbrs) => {
-      if (err)
-        alert(err)
-      else
-        win.webContents.send('ls-mbr', mbrs)
+    .sort({ mbrName: 1 }).exec((err, mbrs) => {
+      win.webContents.send('ls-mbr', mbrs)
     })
 }
 
 function onMbrChanged(err) {
-  if (err)
-    alert(err)
   lsMbr()
 }
 
@@ -133,32 +116,10 @@ function getProfit() {
     let profit_ = null
     if (profit) {
       let mbrProfit = profit['mbrProfit' + workingDay]
-      if (mbrProfit !== undefined)
+      if (mbrProfit)
         profit_ = mbrProfit
     }
     win.webContents.send('get-mbr-profit', profit_)
-  })
-}
-
-function checkHasMbr(_id) {
-  mbrDb.findOne({ _id: _id }, (err, mbr) => {
-    let hasMbr = false
-    for (let mbrRebate of [mbr.mbrRebate1, mbr.mbrRebate2, mbr.mbrRebate3]) {
-      if (!mbrRebate)
-        continue
-      
-      for (let mbrRebate_ of mbrRebate) {
-        if (mbrRebate_.mbrDay === null)
-          continue
-          
-        let mbrPackage = parseFloat(mbrRebate_.mbrPackage)
-        if (!isNaN(mbrPackage) && mbrPackage > 0) {
-          hasMbr = true
-          break
-        }
-      }
-    }
-    win.webContents.send('check-has-mbr', { mbr_id: _id, hasMbr: hasMbr })
   })
 }
 
@@ -258,19 +219,10 @@ ipcMain.on('set-mbr-rebate', (evt, obj) => {
       let updDat = {}
       updDat[key] = mbrRebate
       mbrDb.update({ _id: _id }, { $set: updDat }, {}, (err, numAff) => {
-        if (err)
-          alert(err)
-        else if (!numAff)
-          alert(key + ' not updated!')
-        else
-          checkHasMbr(_id)
+        lsMbr()
       })
     }
   }
-})
-
-ipcMain.on('check-has-mbr', (evt, _id) => {
-  checkHasMbr(_id)
 })
 
 ipcMain.on('open-url', (evt, url) => {
